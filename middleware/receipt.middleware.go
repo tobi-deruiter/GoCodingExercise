5353d381f1dc9df6bc1b61b4a154b1e43de6c5b7
tobi-deruiter <zachderuiter@gmail.com>
2025-04-27 17:58:54 -0400

    finished middleware (error checking)

--- middleware/receipt.middleware.go
+++ middleware/receipt.middleware.go
@@ -1,2 +1,32 @@
 package middleware
 
+import (
+	"GoCodingExercise/models"
+	"encoding/json"
+	"net/http"
+)
+
+/*
+	PUBLIC
+*/
+
+// Check for bad requests, returns status and Receipt if ok; BadRequest if not
+func IsValidReceipt(w http.ResponseWriter, r *http.Request) (int, any) {
+	receiptRequest := models.Receipt{}
+	err := json.NewDecoder(r.Body).Decode(&receiptRequest)
+	if err != nil {
+
+	}
+	return 400, models.NewBadRequest()
+}
+
+// Check if ID is valid, returns status and PointsRequest if ok; NotFound if not
+func IsValidId(w http.ResponseWriter, r *http.Request) (int, any) {
+	id := r.PathValue("id")
+	for _, r := range models.GetReceipts() {
+		if r.Id == id {
+			return 200, models.NewPointsResponse(r.Points)
+		}
+	}
+	return 404, models.NewNotFound()
+}
