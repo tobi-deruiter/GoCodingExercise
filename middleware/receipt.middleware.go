002119baa53387e035acb04897573a3173511e53
tobi-deruiter <zachderuiter@gmail.com>
2025-04-27 20:30:18 -0400

    added regex to confirm receipt format

--- middleware/receipt.middleware.go
+++ middleware/receipt.middleware.go
@@ -3,7 +3,9 @@ package middleware
 import (
 	"GoCodingExercise/models"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"regexp"
 )
 
 /*
@@ -17,6 +19,27 @@ func IsValidReceipt(w http.ResponseWriter, r *http.Request) (int, interface{}) {
 	if err != nil {
 		return 400, models.NewBadRequest()
 	}
+
+	re_retailer := regexp.MustCompile(`^[\w\s\-&]+$`)
+	re_pDate := regexp.MustCompile(`^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$`)
+	re_pTime := regexp.MustCompile(`^([01][0-9]|2[0-4]):([0-5][0-9])$`)
+	re_iDesc := regexp.MustCompile(`^[\w\s\-]+$`)
+	re_removeSpaces := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
+	re_price := regexp.MustCompile(`^\d+\.\d{2}$`)
+
+	receipt.Retailer = re_retailer.FindString(receipt.Retailer)
+	receipt.PurchaseDate = re_pDate.FindString(receipt.PurchaseDate)
+	receipt.PurchaseTime = re_pTime.FindString(receipt.PurchaseTime)
+	receipt.Total = re_price.FindString(receipt.Total)
+
+	for i, item := range receipt.Items {
+		item.ShortDescription = re_removeSpaces.ReplaceAllString(re_iDesc.FindString(item.ShortDescription), "")
+		item.Price = re_price.FindString(item.Price)
+		receipt.Items[i] = item
+	}
+
+	fmt.Println(receipt)
+
 	return 200, receipt
 }
 
