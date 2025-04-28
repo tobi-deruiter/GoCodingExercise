806093c32c8a453b54f10430360a210a064dacf7
tobi-deruiter <zachderuiter@gmail.com>
2025-04-28 19:45:03 -0400

    added print saying server started on port

--- server.go
+++ server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"GoCodingExercise/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+var PORT int = 3000
+
 func main() {
 	http.HandleFunc("/receipt/process", router.ProcessReceipt)
 	http.HandleFunc("/receipt/{id}/points", router.GetPointsById)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	fmt.Printf("Starting server on port %d\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
 }
