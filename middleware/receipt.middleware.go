83a1e3adea5efd4832e8170f4b69beb06ef7ba64
tobi-deruiter <zachderuiter@gmail.com>
2025-04-28 19:46:18 -0400

    fixed IsValidReceipt: now checks each field is not blank after using regex

--- middleware/receipt.middleware.go
+++ middleware/receipt.middleware.go
@@ -3,10 +3,16 @@ package middleware
 import (
 	"GoCodingExercise/models"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"reflect"
 	"regexp"
 )
+
+/*
+	PRIVATE
+*/
+
+// Remove leading/trailing spaces and make sure field is not blank/null
 
 /*
 	PUBLIC
@@ -14,17 +20,19 @@ import (
 
 // Check for bad requests, returns status and Receipt if ok; BadRequest if not
 func IsValidReceipt(w http.ResponseWriter, r *http.Request) (int, interface{}) {
+	// get receipt from request body
 	receipt := models.Receipt{}
 	err := json.NewDecoder(r.Body).Decode(&receipt)
 	if err != nil {
 		return 400, models.NewBadRequest()
 	}
 
+	// check each element that it follows the required format
+	re_removeSpaces := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
 	re_retailer := regexp.MustCompile(`^[\w\s\-&]+$`)
 	re_pDate := regexp.MustCompile(`^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$`)
 	re_pTime := regexp.MustCompile(`^([01][0-9]|2[0-4]):([0-5][0-9])$`)
 	re_iDesc := regexp.MustCompile(`^[\w\s\-]+$`)
-	re_removeSpaces := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
 	re_price := regexp.MustCompile(`^\d+\.\d{2}$`)
 
 	receipt.Retailer = re_retailer.FindString(receipt.Retailer)
@@ -38,7 +46,35 @@ func IsValidReceipt(w http.ResponseWriter, r *http.Request) (int, interface{}) {
 		receipt.Items[i] = item
 	}
 
-	fmt.Println(receipt)
+	// if any field didnt follow the format, it will be left blank
+	// if blank, it as a bad request
+	var s string
+	v := reflect.ValueOf(receipt)
+	for i := range v.NumField() {
+		f := v.Field(i)
+		name := v.Type().Field(i).Name
+		if name != "id" && name != "points" {
+			if f.Type() == reflect.TypeOf(s) {
+				if f.String() == "" {
+					return 400, models.NewBadRequest()
+				}
+			} else {
+				items, _ := f.Interface().([]models.Item)
+				if len(items) == 0 {
+					return 400, models.NewBadRequest()
+				}
+				for j := range items {
+					w := reflect.ValueOf(items[j])
+					for k := range w.NumField() {
+						g := w.Field(k)
+						if g.String() == "" {
+							return 400, models.NewBadRequest()
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return 200, receipt
 }
